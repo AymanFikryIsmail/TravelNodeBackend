adc79d4b3336d672eda7035008dd6c6f3cc049f5
ayman <afekry@splenkid.com>
2019-05-20 11:33:02 +0200

    ayman push

--- routes/package.js
+++ routes/package.js
@@ -6,10 +6,73 @@ var pool = require('../config/config');
 
 var gcm = require('node-gcm');
  */
-
 /* GET packages listing. */
 router.get('/', function(req, res, next) {
     res.send('respond with a resource in packages');
+	});
+router.get('/offers',function(req,res){
+		var sql = "SELECT * from packages where date > CURRENT_TIMESTAMP";
+		pool.query(sql,function(err,result){
+					if(err){
+				res.json({			
+					status : false,
+					data : null,
+					message : err				
+				});			
+			}else{
+				
+				res.json({		
+					status : true,
+					data : result,
+					message : "done"			
+				});		
+				
+			}		
+			
+		});
+	});
+	router.get('/recent',function(req,res){
+		var date = new Date()-2
+		var sql = "SELECT * from packages where addingDate  >= ? and date > CURRENT_TIMESTAMP ";
+		pool.query(sql,[date],function(err,result){
+					if(err){
+				res.json({			
+					status : false,
+					data : null,
+					message : err				
+				});			
+			}else{
+				
+				res.json({		
+					status : true,
+					data : result,
+					message : "done"			
+				});		
+				
+			}		
+			
+		});
+	});
+	router.get('/recommended',function(req,res){
+		var sql = "SELECT * from packages where packages.cid=(SELECT company.cid from company where rate>=4) and date > CURRENT_TIMESTAMP ";
+		pool.query(sql,function(err,result){
+					if(err){
+				res.json({			
+					status : false,
+					data : null,
+					message : err				
+				});			
+			}else{
+				
+				res.json({		
+					status : true,
+					data : result,
+					message : "done"			
+				});		
+				
+			}		
+			
+		});
 	});
 router.get('/city',function(req,res){
 	var sql = "SELECT photo_path , travel_to , p_name from packages , package_photo  where date >= CURRENT_TIMESTAMP and package_photo.pid=packages.pid";
@@ -32,11 +95,9 @@ router.get('/city',function(req,res){
 		
 	});
 });
-
-
 router.get('/city/packages',function(req,res){
 	var city =req.query.city
-	var sql = "SELECT * from packages WHERE travel_to=?" ;      //search by travel to
+	var sql = "SELECT * from packages WHERE travel_to=? and date >= CURRENT_TIMESTAMP" ;
 	pool.query(sql,[city],function(err,result){
 				if(err){
 			res.json({			
@@ -60,8 +121,14 @@ router.get('/city/packages',function(req,res){
 router.get('/search',function(req,res){
 	var travelFrom =req.query.from
 	var travelTo =req.query.to
-	var values = [travelFrom, travelTo]
-	var sql = "SELECT * from packages WHERE travel_from =? and travel_to =?" ;
+	var dateFrom = req.query.dateFrom
+	var dateTo = req.query.dateTo
+	var values = [travelFrom, travelTo, dateFrom, dateTo]
+	if(dateFrom && dateTo){
+		var sql = "SELECT * from packages WHERE travel_from =? and travel_to =? and date >= CURRENT_TIMESTAMP and date between ? and ?" ;
+	} else {
+		var sql = "SELECT * from packages WHERE travel_from =? and travel_to =? and date >= CURRENT_TIMESTAMP" ;
+	}
 	pool.query(sql,values,function(err,result){
 				if(err){
 			res.json({			
@@ -81,9 +148,8 @@ router.get('/search',function(req,res){
 		
 	});
 });
-
 router.get('/search/from',function(req,res){
-	var sql = "SELECT travel_from from packages" ;
+	var sql = "SELECT travel_from from packages where date >= CURRENT_TIMESTAMP" ;
 	pool.query(sql,function(err,result){
 				if(err){
 			res.json({			
@@ -104,7 +170,7 @@ router.get('/search/from',function(req,res){
 	});
 });
 router.get('/search/to',function(req,res){
-	var sql = "SELECT travel_to from packages" ;
+	var sql = "SELECT travel_to from packages where date >= CURRENT_TIMESTAMP" ;
 	pool.query(sql,function(err,result){
 				if(err){
 			res.json({			
@@ -134,7 +200,7 @@ router.get('/filter',function(req,res){
 	var maxDays =req.query.maxDays
 	var minRate = req.query.rate
 	var values = [travelFrom, travelTo, minPrice, maxPrice, minDays ,maxDays, minRate]
-	var sql = "SELECT * from packages WHERE travel_from =? and travel_to =? and (price between ? and ?) and (duration between ? and ?) and packages.cid=(SELECT company.cid from company where rate>=?)" ;
+	var sql = "SELECT * from packages WHERE travel_from =? and travel_to =? and (price between ? and ?) and (duration between ? and ?) and packages.cid=(SELECT company.cid from company where rate>=?) and date >= CURRENT_TIMESTAMP" ;
 	pool.query(sql,values,function(err,result){
 				if(err){
 			res.json({			
